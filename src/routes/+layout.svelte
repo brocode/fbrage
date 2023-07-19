af251ea3c2146f03168ac93951d4fae842959f4f
Patrick Haun <patrick@bomgar.email>
2023-07-19 16:27:43 +0200

    move stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,16 @@
   import "./global.scss";
 </script>
 
-<ul>
-  <li>
-    <a href="/private-key">Private key management</a>
-  </li>
-  <li>
-    <a href="/test">Test</a>
-  </li>
-</ul>
+<nav>
+  <ul>
+    <li>
+      <a href="/private-key">Private key management</a>
+    </li>
+    <li>
+      <a href="/test">Test</a>
+    </li>
+  </ul>
+</nav>
 
 <div>
   <slot />
