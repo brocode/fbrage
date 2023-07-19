082ff6728fb9a08b7621d270532dcce45f086355
Patrick Haun <patrick@bomgar.email>
2023-07-19 20:05:53 +0200

    [wip] decrypt

--- rage-webassembly/src/ageutil.rs
+++ rage-webassembly/src/ageutil.rs
@@ -1,7 +1,33 @@
 use std::str::FromStr;
+use std::sync::Arc;
 
-use age::x25519::Identity;
+use age::armor::ArmoredReader;
+use age::Decryptor;
+use age::Identity;
+use std::io::{Cursor, Read};
 
+pub fn decrypt_message(
+    identities: Vec<Arc<dyn Identity>>,
+    message: &str,
+) -> Result<String, String> {
+    let input = Cursor::new(message);
+
+    match Decryptor::new(ArmoredReader::new(input)).map_err(|e| e.to_string())? {
+        Decryptor::Recipients(d) => {
+            let mut reader = d
+                .decrypt(identities.iter().map(|arc| arc.as_ref()))
+                .map_err(|e| e.to_string())?;
+            let mut buffer = String::new();
+            reader
+                .read_to_string(&mut buffer)
+                .map_err(|e| e.to_string())?;
+            return Ok(buffer);
+        }
+        Decryptor::Passphrase(_) => {
+            return Err("Passphrase based decryption not supported".to_string());
+        }
+    }
+}
 fn remove_comments(input: &str) -> String {
     input
         .lines()
@@ -10,10 +36,13 @@ fn remove_comments(input: &str) -> String {
         .join("\n")
 }
 
-pub fn parse_private_keys(keys: Vec<String>) -> Result<Vec<Identity>, String> {
+pub fn parse_private_keys(keys: Vec<String>) -> Result<Vec<Arc<dyn Identity>>, String> {
     let identities: Vec<_> = keys
         .iter()
-        .map(|key| Identity::from_str(&remove_comments(key)))
+        .map(|key| {
+            age::x25519::Identity::from_str(&remove_comments(key))
+                .map(|id| Arc::new(id) as Arc<dyn Identity>)
+        })
         .collect::<Result<Vec<_>, _>>()?;
     return Ok(identities);
 }
@@ -25,7 +54,7 @@ mod tests {
 
     #[test]
     fn test_parse_keys() -> Result<(), String> {
-        let identity = Identity::generate();
+        let identity = age::x25519::Identity::generate();
         let key_as_string: String = format!(
             "# created: 2023-07-12T12:16:23+02:00\n{}\n",
             identity.to_string().expose_secret().to_owned()
