d40f1266da96b7509f5538964d181eff42b2bf2e
Patrick Haun <patrick@bomgar.email>
2023-07-19 16:57:32 +0200

    basic menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,21 @@
   import "./global.scss";
 </script>
 
-<nav>
-  <ul>
-    <li>
-      <a href="/private-key">Private key management</a>
-    </li>
-    <li>
-      <a href="/test">Test</a>
-    </li>
-  </ul>
-</nav>
-
-<div>
+<div class="title">
+  <a href="/">fbrage</a>
+</div>
+<div class="content">
   <slot />
 </div>
+
+<style lang="scss">
+  .title {
+    display: flex;
+    justify-content: center;
+    font-size: 2rem;
+    font-weight: bold;
+  }
+  .content {
+    padding: 10px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,18 @@
-Startseite
+<div class="wrapper">
+  <div class="main">
+    <a role="button" href="/test">Test</a>
+    <a role="button" href="/private-key">Private key management</a>
+  </div>
+</div>
+
+<style lang="scss">
+  .wrapper {
+    display: flex;
+    justify-content: center;
+  }
+  .main {
+    display: grid;
+    gap: 10px;
+    max-width: 600px;
+  }
+</style>
