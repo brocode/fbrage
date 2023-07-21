ee3568d060708091681bc14113b80359f269da8c
Patrick Haun <patrick@bomgar.email>
2023-07-21 16:54:09 +0200

    fix pre

--- src/routes/public-keys/+page.svelte
+++ src/routes/public-keys/+page.svelte
@@ -4,15 +4,19 @@
 </script>
 
 <h1>Coole public keys</h1>
+
+<p>Currently only the import of symbiolab age public keys is supported. Manual key management pending.</p>
+<p>
+  <strong>Beware: An import replaces all keys with the imported ones.</strong>
+</p>
+
 <ImportPublicKeys />
 
 {#each Object.keys($publicKeyStore) as recipientName}
   <article>
     <header>{recipientName}</header>
     {#each $publicKeyStore[recipientName] as key}
-      <pre>
-        {key}
-      </pre>
+      <pre>{key}</pre>
     {/each}
   </article>
 {/each}
