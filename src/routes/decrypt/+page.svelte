b430d726e7522db78bcc6da6a4fab274cd483bcb
Patrick Haun <patrick@bomgar.email>
2023-08-04 10:56:24 +0200

    decrypt form was useless

--- src/routes/decrypt/+page.svelte
+++ src/routes/decrypt/+page.svelte
@@ -1,8 +1,36 @@
 <script lang="ts">
-  import DecryptForm from "$lib/components/DecryptForm.svelte";
+  import ErrorMessage from "$lib/components/ErrorMessage.svelte";
+  import Message from "$lib/components/Message.svelte";
   import { privateKeyStore } from "$lib/private_key_store";
+  import { decrypt_message } from "rage-webassembly";
+  import { get } from "svelte/store";
+
+  let plainText: string | null = null;
+  let error: string | null = null;
+
+  let cipherText = "";
+
+  const handleSubmit = () => {
+    error = null;
+    plainText = null;
+    try {
+      plainText = decrypt_message(cipherText, Object.values(get(privateKeyStore)));
+    } catch (e: unknown) {
+      error = String(e);
+    }
+  };
 </script>
 
 <p>Available private keys: {Object.keys($privateKeyStore).length}</p>
 
-<DecryptForm privateKeyStore={$privateKeyStore} />
+<form on:submit|preventDefault={handleSubmit}>
+  <textarea required rows={10} placeholder="Ciphertext" bind:value={cipherText} />
+  <button>Decrypt</button>
+</form>
+
+{#if plainText != null}
+  <Message message={plainText} />
+{/if}
+{#if error != null}
+  <ErrorMessage {error} />
+{/if}
