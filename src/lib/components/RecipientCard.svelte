7c0d64d39ccbe2a3dda9381491809e94eef596d7
Patrick Haun <patrick@bomgar.email>
2023-08-04 10:28:41 +0200

    recipient card

---
+++ src/lib/components/RecipientCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { publicKeyStore } from "$lib/public_key_store";
+  export let recipientName: string;
+</script>
+
+<article>
+  <header>{recipientName}</header>
+  {#each $publicKeyStore[recipientName] as key}
+    <pre>{key}</pre>
+  {/each}
+</article>
--- src/routes/public-keys/+page.svelte
+++ src/routes/public-keys/+page.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
   import CreateRecipientForm from "$lib/components/CreateRecipientForm.svelte";
   import ImportPublicKeys from "$lib/components/ImportPublicKeys.svelte";
+  import RecipientCard from "$lib/components/RecipientCard.svelte";
   import { publicKeyStore } from "$lib/public_key_store";
 </script>
 
 <h1>Existing public keys</h1>
 
 {#each Object.keys($publicKeyStore) as recipientName}
-  <article>
-    <header>{recipientName}</header>
-    {#each $publicKeyStore[recipientName] as key}
-      <pre>{key}</pre>
-    {/each}
-  </article>
+  <RecipientCard {recipientName} />
 {/each}
 
 <h1>Recipients</h1>
